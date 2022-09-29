f33216a001cb6960befa42d73ee09ffa1a988f69
SimPilotAdamT <adam_tazul@outlook.com>
2022-09-29 15:21:12 +0100

    Refactored maths.java

--- src/com/AdamT/maths.java
+++ src/com/AdamT/maths.java
@@ -1,6 +1,10 @@
 package com.AdamT;
 
 import  java.lang.*;
-import java.util.*;
 
-class maths {public static String sum(int x,int y){int z=x+y;return("\nSum of x+y = "+z+"\n");}}
+class maths {
+	public static String sum(int x, int y) {
+		int z = x + y;
+		return("\nSum of x+y = " + z + "\n");
+	}
+}
