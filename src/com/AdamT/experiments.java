78d4efc517eff6070c2178b57bc16071e4c93312
SimPilotAdamT <adam_tazul@outlook.com>
2022-09-29 15:45:52 +0100

    Refactoring

--- src/com/AdamT/experiments.java
+++ src/com/AdamT/experiments.java
@@ -7,7 +7,7 @@ public class experiments {
     static Scanner input = new Scanner(System.in);
     static String inp;
     static boolean valid;
-    public static void main(String[] args){
+    public static void main(String[] args) {
         boolean exit = false;
         System.out.println("Welcome to AdamT's collection of programs written for UEL!\n");
         while (!exit) {
@@ -15,7 +15,7 @@ public class experiments {
             inp = input.nextLine();
             valid = false;
             while (!valid){
-                if (!isInteger(inp)){
+                if (isNotInteger(inp)) {
                     System.out.println("\nERROR!\nInput is not an integer!!\n");
                     menu();
                     inp = input.nextLine();
@@ -47,13 +47,13 @@ public class experiments {
         System.out.println("3: Exit (exit this program)");
         System.out.print("\nPlease input the corresponding number to your desired option: ");
     }
-    private static boolean isInteger(String input){try{Integer.parseInt(input);return true;}catch(Exception ignored){return false;}}
+    private static boolean isNotInteger(String input){try{Integer.parseInt(input);return false;}catch(Exception ignored){return true;}}
     private static int impInt() {
         System.out.print("\nEnter an integer: ");
         inp = input.nextLine();
         valid = false;
         while (!valid) {
-            if (!isInteger(inp)) {
+            if (isNotInteger(inp)) {
                 System.out.println("\nERROR!\nInput not an integer!\n");
                 System.out.print("Try again: ");
                 inp = input.nextLine();
--- src/com/AdamT/maths.java
+++ src/com/AdamT/maths.java
@@ -4,7 +4,11 @@ import  java.lang.*;
 
 class maths {
 	public static String sum(int x, int y) {
-		int z = x + y;
-		return("\nSum of x+y = " + z + "\n");
+		try {
+			int z = x + y;
+			return("\nSum of x+y = " + z + "\n");
+		} catch (Exception  ignored) {
+			return("\nERROR!\nThere was an internal error with the calculation.\n");
+		}
 	}
 }
